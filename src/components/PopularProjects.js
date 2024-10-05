2bcc4578e6ef934910ee6ef222c060f25be92934
varun teja boorla <64516785+teju001@users.noreply.github.com>
2024-10-05 15:41:57 -0400

    Update PopularProjects.js

--- src/components/PopularProjects.js
+++ src/components/PopularProjects.js
@@ -1,46 +1,56 @@
 import React from 'react';
 import './PopularProjects.css';
 
+// Import the images
+import furnitureAssembly from './Assets/furniture-assembly.jpg';
+import mountArt from './Assets/mount-art.jpg';
+import mountTv from './Assets/mount-tv.jpg';
+import helpMoving from './Assets/help-moving.jpg';
+import cleaning from './Assets/cleaning.jpg';
+import plumbing from './Assets/plumbing.jpg';
+import electrical from './Assets/electrical.jpg';
+import heavyLifting from './Assets/heavy-lifting.jpg';
+
 const projectsData = [
   {
     title: "Furniture Assembly",
     price: "$49",
-    image: "./Assets/furniture-assembly.jpg"  
+    image: furnitureAssembly  // Use the imported image
   },
   {
     title: "Mount Art or Shelves",
     price: "$65",
-    image: "./Assets/mount-art.jpg"
+    image: mountArt
   },
   {
     title: "Mount a TV",
     price: "$69",
-    image: "./Assets/mount-tv.jpg"
+    image: mountTv
   },
   {
     title: "Help Moving",
     price: "$67",
-    image: "./Assets/help-moving.jpg"
+    image: helpMoving
   },
   {
     title: "Home & Apartment Cleaning",
     price: "$49",
-    image: "./Assets/cleaning.jpg"
+    image: cleaning
   },
   {
     title: "Minor Plumbing Repairs",
     price: "$74",
-    image: "./Assets/plumbing.jpg"
+    image: plumbing
   },
   {
     title: "Electrical Help",
     price: "$69",
-    image: "./Assets/electrical.jpg"
+    image: electrical
   },
   {
     title: "Heavy Lifting",
     price: "$61",
-    image: "./Assets/heavy-lifting.jpg"
+    image: heavyLifting
   }
 ];
 
